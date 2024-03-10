124d4510f6ee74c1be18f266821e93e8e17e7b51
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-03-10 16:11:13 +0200

    Add projectService.js

---
+++ src/utils/projectService.js
@@ -0,0 +1,38 @@
+import { Project } from "../projectsAndTodos/Project.js";
+import { v4 as uuid } from "uuid";
+
+const getProjectTitles = (projects) => projects.map((project) => project.title);
+
+const createNewProject = (projects, projectTitle) => {
+  if (getProjectByTitle(projects, projectTitle)) {
+    console.log(`Project named '${projectTitle}' already exists!`);
+    return;
+  }
+  console.log(`Add new project: ${projectTitle}`);
+  projects.push(new Project(uuid(), projectTitle));
+  return projects;
+};
+
+const removeProject = (projects, projectTitle) => {
+  console.log(`Remove project: ${projectTitle}`);
+  return projects.filter((project) => project.title !== projectTitle);
+};
+
+const getProjectByTitle = (projects, projectTitle) =>
+  projects.find((project) => project.title === projectTitle);
+
+const renameProject = (projects, oldTitle, newTitle) =>
+  projects.map((project) => {
+    if (project.title === oldTitle) {
+      project.title = newTitle;
+    }
+    return project;
+  });
+
+export {
+  getProjectTitles,
+  createNewProject,
+  removeProject,
+  getProjectByTitle,
+  renameProject,
+};
