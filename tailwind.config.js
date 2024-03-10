59531a34aaa6b6a56b449653df2aca217c64ed9a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-03-10 16:10:41 +0200

    Update to use ES6-modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   content: ["./dist/*.html"],
   theme: {
     extend: {},
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,7 @@
-const { merge } = require("webpack-merge");
-const path = require("path");
-const common = require("./webpack.common.js");
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-module.exports = merge(common, {
+export default merge(common, {
   mode: "development",
   devtool: "inline-source-map",
 });
