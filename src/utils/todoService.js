8f64aa28ec31658305e42b0c40f11fb9cb73baaa
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-03-10 16:34:58 +0200

    Add todoService.js

---
+++ src/utils/todoService.js
@@ -0,0 +1,29 @@
+import { format, isWithinInterval } from "date-fns";
+
+const sortTodos = (todos, key) => {
+  return todos.sort((a, b) => (a[key] <= b[key] ? -1 : 1));
+};
+
+const getAllTodos = (projects) => {
+  let allTodos = [];
+  projects.forEach((project) => {
+    allTodos = [...allTodos, ...project.getAllTodos()];
+  });
+  return sortTodos(allTodos, "dueDate");
+};
+
+const getTodosDueBy = (projects, dueDate) => {
+  console.log(`Todos due: ${format(dueDate, "yyyy-MM-dd")}`);
+  const allToDos = getAllTodos(projects);
+
+  const todosDueBy = allToDos.filter((todo) =>
+    isWithinInterval(todo.dueDate, {
+      start: new Date(),
+      end: dueDate,
+    })
+  );
+
+  return sortTodos(todosDueBy, "dueDate");
+};
+
+export { getAllTodos, getTodosDueBy };
